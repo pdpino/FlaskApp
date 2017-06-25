4f17ba64ee0cfa93c4cdcce7f118932f5959b8b3
Pablo Pino <pdpino@uc.cl>
2017-06-25 01:27:19 -0400

    try fix

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -84,6 +84,7 @@ def search_by_date():
     if date is None:
         return "[]" # No query
 
+    return date
     results = mongodb.colEscuchas.find({"fecha": date}, {"_id":0, "numero":1})
     results = json_util.dumps(results, sort_keys=True, indent=4)
     return str(results) # return plain string
